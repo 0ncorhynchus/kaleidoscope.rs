9cecc7bae170e301840e5b52c08c798cc98cfbf6
Suguru Kato <salmon.bioinformatics@gmail.com>
2020-02-08 15:20:10 +0900

    :construction: Add Token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[derive(Clone, Debug, PartialEq)]
+enum Token {
+    EOF,
+    Def,
+    Extern,
+    Identifier(String), // IdentifierStr
+    Number(f64),        // NumVal
+}
+
 fn main() {
     println!("Hello, world!");
 }
