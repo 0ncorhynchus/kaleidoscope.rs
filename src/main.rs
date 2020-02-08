ee96f813d940d5595f908f85c54b89313495cbdb
Suguru Kato <salmon.bioinformatics@gmail.com>
2020-02-08 15:58:21 +0900

    :construction: Add Lexer

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,28 @@ enum Token {
     Number(f64),        // NumVal
 }
 
+struct Lexer<I> {
+    iter: I,
+    last_char: Option<char>,
+}
+
+impl<I> Lexer<I>
+where
+    I: Iterator<Item = char>,
+{
+    fn new(iter: I) -> Self {
+        let mut iter = iter;
+        let last_char = iter.next();
+        Self { iter, last_char }
+    }
+
+    fn get_char(&mut self) -> Option<char> {
+        let c = self.last_char;
+        self.last_char = self.iter.next();
+        c
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
